74f637f4f2fa4ffb86047e4a81fc9d8a3c96749d
Jan Willems <jw@elevenbits.com>
2017-01-26 21:24:15 +0100

    Added languages: NL or EN. Updated the about look and feel. Adapted the home according to clients wishes.

--- imagery/impart/views.py
+++ imagery/impart/views.py
@@ -4,8 +4,12 @@ from django.db.models import Q
 
 from imagery.impart.models import News, LandPrice, Manifest, Artist, Art, Content, Contact
 
+import collections
 import logging
 logger = logging.getLogger("imagery")
+
+
+Language = collections.namedtuple('Language', 'code logo name other')
 
 
 def archive(request, page=1):
@@ -23,6 +27,17 @@ def archive(request, page=1):
     return render(request, 'pages/archive.html', attributes)
 
 
+def get_language(request):
+    """Get the selected language.
+
+    English is the default."""
+    code = request.GET.get('language', 'EN')
+    if code == 'NL':
+        return Language('NL', 'uk.svg', 'English', 'EN')
+    else:
+        return Language('EN', 'belgium.svg', 'Nederlands', 'NL')
+
+
 def artist(request, artist_id):
     """Show artist information: about, art and contact."""
 
@@ -33,18 +48,14 @@ def artist(request, artist_id):
         raise Http404("Sorry, but that artist does not exist!")
     logger.info("Showing details of " + artist.name + ".")
 
-    # get his/her content...
-    about_en_contents = Content.objects.filter(
-        Q(section='AA'), Q(language='EN'), Q(identifier=artist.link)
+    # get his/her content based on the selected language...
+    language = get_language(request)
+    logger.info(language)
+    about_contents = Content.objects.filter(
+        Q(section='AA'), Q(language=language.code), Q(identifier=artist.link)
     )
-    about_nl_contents = Content.objects.filter(
-        Q(section='AA'), Q(language='NL'), Q(identifier=artist.link)
-    )
-    contact_en_contents = Content.objects.filter(
-        Q(section='CO'), Q(language='EN'), Q(identifier=artist.link)
-    )
-    contact_nl_contents = Content.objects.filter(
-        Q(section='CO'), Q(language='NL'), Q(identifier=artist.link)
+    contact_contents = Content.objects.filter(
+        Q(section='CO'), Q(language=language.code), Q(identifier=artist.link)
     )
 
     # ...and other entities
@@ -65,10 +76,10 @@ def artist(request, artist_id):
     logger.info("Art labels: {}.".format(art_labels))
 
     attributes = {'menu': 'artist',
-                  'about_en_contents': about_en_contents,
-                  'about_nl_contents': about_nl_contents,
-                  'contact_en_contents': contact_en_contents,
-                  'contact_nl_contents': contact_nl_contents,
+                  'artist': artist,
+                  'language': language,
+                  'about_contents': about_contents,
+                  'contact_contents': contact_contents,
                   'works': works,
                   'art_labels': art_labels,
                   'contact': contact}
@@ -90,6 +101,8 @@ def index(request):
         Q(section='HO'), Q(language='NL')
     )
 
+    language = get_language(request)
+
     # ...and all other entities
     artists = Artist.objects.filter(active=True)
     news = News.objects.filter(active=True).reverse()
@@ -97,6 +110,7 @@ def index(request):
     prices = LandPrice.objects.filter(active=True)
 
     attributes = {'menu': 'home',
+                  'language': language,
                   'home_en_contents': home_en_contents,
                   'home_nl_contents': home_nl_contents,
                   'artists': artists,
